d328a90b1df585623de3386d1268b91cceb0d25b
KoteswariM <koteswarimadineni477_@email.com>
2021-11-21 17:30:21 +0530

    add icons
    -icons added in book-details.component.scss

--- B16BBookKart/src/app/app.module.ts
+++ B16BBookKart/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
