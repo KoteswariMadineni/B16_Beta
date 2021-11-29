ac61b922dc9877db4bce64c9be91d3c9b381593b
KoteswariM <koteswarimadineni477_@email.com>
2021-11-29 14:24:43 +0530

    commit changes

--- B16BBookKart/src/app/book-details/book-details.component.ts
+++ B16BBookKart/src/app/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BookDetailsService } from './book-details.service';
-//import { IBookDetails } from './services/book-details';
+
 
 @Component({
   selector: 'app-book-details',
@@ -9,18 +9,11 @@ import { BookDetailsService } from './book-details.service';
 })
 export class BookDetailsComponent implements OnInit {
     public bookDetails:any;
-   //public errMsg: any;
+   
 
   constructor(private detailsService: BookDetailsService) { }
 
   ngOnInit(): void {
-    
-  /*this.detailsService.getBookDetails().subscribe
-     (
-       data=>this.bookDetails=data
-      //error=> this.errMsg= error
- ) ;*/
- 
     
   this.detailsService.getBookDetailsByParameter().subscribe(data=>{this.bookDetails=data;});
   }
